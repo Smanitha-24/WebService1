4418461da3fe3a0051a7bbc6f1eb5cae0c49b844
Smanitha <smanitha@takyonline.com>
2024-11-12 06:26:43 +0530

    Deleted unwanted files

--- InputModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SalesTaxService.Models
-{
-    public class InputModel
-    {
-        public string Text { get; set; } = string.Empty;
-        public decimal TaxRate { get; set; } = 10;
-    }
-}
