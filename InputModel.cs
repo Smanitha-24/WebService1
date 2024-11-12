71227188088bf6f5680fe22d4ac6873bc4dbf3ec
Smanitha-24 <reachsmani@gmail.com>
2024-11-12 06:17:42 +0530

    Project structure correction - Models

---
+++ InputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesTaxService.Models
+{
+    public class InputModel
+    {
+        public string Text { get; set; } = string.Empty;
+        public decimal TaxRate { get; set; } = 10;
+    }
+}
